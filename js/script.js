3725f7f385fe2fd4179e6265dfe3c0453a08b685
kirill <lolik@gmail.com>
2020-07-31 23:45:56 +0300

    commit after creating console 4 questions about movies

--- js/script.js
+++ js/script.js
@@ -1,46 +1,20 @@
-"use strict";
-let number = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-number = 10;
-console.log(number);
-
-const obj = {
-    name: "John",
-    age: 25,
-    isHeMarried: false
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-console.log(obj.name);  // Лучше этим способом
 
-console.log(obj ["name"]); 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, ];
-console.log(arr[1]);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-alert('Hello');
-
-
-const result = confirm("Are you here???");
-console.log(result);
-
-const answer = prompt("Вам есть 18?", "18");
-console.log(answer); // Пользователь вводит в ответе только тип данных строка. ВСЯ ИНФА КАК ПРАВИЛО ОТ ПОЛЬЗОВАТЕЛЯ ПРИХОДИТ В ТИПЕ ДАННЫХ "СТРОКА"
-
-const answers = [];
-
-answers[0] = prompt('Как вас зовут?', '');
-answers[1] = prompt('Ваша фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
-
-document.write(answers);
-
-const category = 'toys';
-
-console.log(`https://someurl.com/${category}/5`); // Чтобы использовать переменные (интерполяция), надо поставить косые тильдовые кавычки
-
-const user = "Ivan";
-
-alert(`Привет, ${user}`);
-
-lol
+console.log(personalMovieDB);
